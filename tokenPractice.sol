c16ef60abe23eca7a0634e7c215e4ca92f67a0a4
hose <mhs0272@naver.com>
2022-12-08 23:40:05 +0900

    erc20

--- tokenPractice.sol
+++ tokenPractice.sol
@@ -1,10 +1,22 @@
+// contracts/GLDToken.sol
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract GLDToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Gold", "GLD") {
+contract testToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("Test", "TST") {
         _mint(msg.sender, initialSupply);
     }
+
+function Deposit() public returns(boolean) {
+
+
 }
 
+
+
+
+
+
+}
