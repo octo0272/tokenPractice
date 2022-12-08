b6c6bb9df4ef6cc73a265cb8208219d92bf3a780
hose <mhs0272@naver.com>
2022-12-08 23:57:44 +0900

    ERC20Test

--- tokenPractice.sol
+++ tokenPractice.sol
@@ -4,19 +4,38 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+uint256 pool;
+address[] Depositor;
+uint256[] Deposit;
+
 contract testToken is ERC20 {
     constructor(uint256 initialSupply) ERC20("Test", "TST") {
         _mint(msg.sender, initialSupply);
     }
 
-function Deposit() public returns(boolean) {
+    function Deposit(uint256 amount) public payable {
+        pool += amount;
+        for(uint i=0; i<length.Depositor; i++){
+            if(Depositor[i]==msg.sender){
+                Deposit[i] += amount;
+            } else {
+                Depositor[length.Depositor]==msg.sender;
+                Deposit[length.Depositor] += amount;
+            }
+        }
+    }
 
-
-}
-
-
-
-
-
-
+    function Withdraw(uint256 amount) public payable {
+        for(uint i=0; i<length.Depositor; i++){
+            if(Depositor[i]==msg.sender){
+                if(Deposit[i]>=amount){
+                    msg.sender.transfer(amount);
+                    pool -= amount;
+                    Deposit[i] -= amount;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
 }
